b9f42454d401e020bc324fd0af8911f37f86addb
federica_di_monaco <fededimonaco@gmail.com>
2021-11-03 20:14:47 +0100

    update component background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { View, Image, ImageBackground, Text } from 'react-native'
+
+import BackgroundGeneric from "./components/backgroundGeneric/BackgroundGeneric";
 import { ButtonGeneric } from "./components";
 
-import { View, Image, ImageBackground } from 'react-native'
-
-import octopusImg from './assets/imgages/octopus.png'
-
+import bg from './assets/imgages/bg.jpg'
+import octopusCard from './assets/imgages/octopusCard.png'
+import lobster from './assets/imgages/lobster.png'
+import murayeel from './assets/imgages/murayeel.png'
 
 const App = () => {
   return (
-    <View style={{ flex: 1}}>
-      <ButtonGeneric
-        styleImg={{ width: 50, height: 50 }}
-        style={{background:'trasparent'}}
-        // title = 'ciao'
-        source={octopusImg}
+    <View>
+
+      <BackgroundGeneric
+        titlePage='The Circle Of Life'
+        source={bg}
+        content={
+          <>
+            <ButtonGeneric style={{ background: 'transparent' }} styleImg={{ width: 100, height: 100 }} source={octopusCard} />
+            <View style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between', alignItems: 'center' }}>
+              <ButtonGeneric style={{ display: 'flex',  marginRight: 100, alignItems: 'center', justifyContent: 'center', }} styleImg={{ width: 100, height: 100 }} source={lobster} />
+              <ButtonGeneric style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} styleImg={{ width: 100, height: 100 }} source={murayeel} />
+            </View>
+            <ButtonGeneric title='PLAY' />
+
+          </>
+        }
       />
     </View>
   );
